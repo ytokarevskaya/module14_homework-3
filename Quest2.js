03d42f81ae915a63c1268b359ad751c1f058eed3
Bloommmy <51096850+Bloommmy@users.noreply.github.com>
2020-08-14 14:45:20 +0300

    Create Quest2.js

---
+++ Quest2.js
@@ -0,0 +1,21 @@
+// Задание 2
+
+const jsonString = `
+{
+  "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}
+`;
+
+const data = JSON.parse(jsonString);
+console.log('result', data);
